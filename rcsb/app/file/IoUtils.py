70af1698dfb9076fac60fb662b82ce54133a9111
Connor Parker <cparker@n7220-owl217-17.rad.rutgers.edu>
2022-02-22 11:20:41 -0500

    Updated IoUtils.py, removed print statement used for debugging

--- rcsb/app/file/IoUtils.py
+++ rcsb/app/file/IoUtils.py
@@ -169,7 +169,7 @@ class IoUtils:
                 logger.info("Path exists (overwrite %r): %r", allowOverWrite, outPath)
                 return {"success": False, "statusCode": 405, "statusMessage": "Encountered existing file - overwrite prohibited"}
             ret = await self.store(ifh, outPath, mode="wb", copyMode=copyMode, hashType=hashType, hashDigest=hashDigest)
-         #
+        #
         ret["fileName"] = os.path.basename(outPath) if ret["success"] else None
         return ret
 
