2e9f389cf098dc8149c7a961c805e4e8bf91ab68
j-s-135 <james.smith@rcsb.org>
2023-09-23 16:52:49 -0400

    DAOTHER-8059 continued

--- rcsb/app/tests-file/testLocking.py
+++ rcsb/app/tests-file/testLocking.py
@@ -19,116 +19,116 @@ class LockTest(unittest.IsolatedAsyncioTestCase):
 
     def testRedisLock(self):
         logging.info("----- TESTING REDIS LOCK -----")
-        asyncio.run(self.testLock(lock=redisLock))
+        asyncio.run(testLock(self, lock=redisLock))
 
     def testTernaryLock(self):
         logging.info("---- TESTING TERNARY LOCK ----")
-        asyncio.run(self.testLock(lock=ternaryLock))
+        asyncio.run(testLock(self, lock=ternaryLock))
 
     def testSoftLock(self):
         logging.info("---- TESTING SOFT LOCK ----")
-        asyncio.run(self.testLock(lock=softLock))
+        asyncio.run(testLock(self, lock=softLock))
 
-    async def testLock(self, lock):
-        testVal = -1
-        repositoryType = "unit-test"
-        depId = "D_000"
-        contentType = "model"
-        milestone = "upload"
-        partNumber = 1
-        contentFormat = "pdbx"
-        version = 1
-        folder = PathProvider().getDirPath(repositoryType, depId)
-        self.assertTrue(folder is not None, "error - could not create folder")
-        logging.info("folder %s", folder)
-        filename = PathProvider().getFileName(
-            depId, contentType, milestone, partNumber, contentFormat, version
-        )
-        self.assertTrue(filename is not None, "error - could not create file name")
-        logging.info("file name %s", filename)
-        filepath = os.path.join(folder, filename)
-        logging.info("file path %s", filepath)
-        # test two writers
-        logging.info("TESTING TWO WRITERS")
-        try:
-            async with lock(filepath, "w", timeout=60):
-                testVal = 1
-                try:
-                    async with lock(filepath, "w", timeout=5):
-                        testVal = 0
-                except (FileExistsError, OSError) as err:
-                    logging.warning("writer error %r", err)
-                self.assertFalse(testVal == 0, "write lock error - test val = 0")
-                self.assertTrue(
-                    testVal == 1, "write lock error - test val %d" % testVal
-                )
-        except (FileExistsError, OSError) as err:
-            logging.warning("writer error %r", err)
-        # test two readers
-        logging.info("TESTING TWO READERS")
-        version = 2
-        filename = PathProvider().getFileName(
-            depId, contentType, milestone, partNumber, contentFormat, version
-        )
-        filepath = os.path.join(folder, filename)
-        try:
-            # second traversal required to detect simultaneity otherwise ternary lock has infinite loop
-            async with lock(filepath, "r", timeout=60, second_traversal=True):
-                testVal = 1
-                try:
-                    async with lock(filepath, "r", timeout=5, second_traversal=True):
-                        testVal = 0
-                except (FileExistsError, OSError) as err:
-                    logging.warning("reader error %r", err)
-                self.assertFalse(testVal == 1, "read lock error - test val = 1")
-                self.assertTrue(
-                    testVal == 0, "read lock error - non-zero test val %d" % testVal
-                )
-        except (FileExistsError, OSError) as err:
-            logging.warning("reader error %r", err)
-        # test reader writer
-        logging.info("TESTING READER WRITER")
-        version = 3
-        filename = PathProvider().getFileName(
-            depId, contentType, milestone, partNumber, contentFormat, version
-        )
-        filepath = os.path.join(folder, filename)
-        try:
-            # second traversal required to detect simultaneity otherwise ternary lock has infinite loop
-            async with lock(filepath, "r", timeout=60, second_traversal=True):
-                testVal = 1
-                try:
-                    async with lock(filepath, "w", timeout=5, second_traversal=True):
-                        testVal = 0
-                except (FileExistsError, OSError) as err:
-                    logging.warning("reader error %r", err)
-                self.assertFalse(testVal == 0, "read lock error - test val = 0")
-                self.assertTrue(
-                    testVal == 1, "read lock error - test val %d" % testVal
-                )
-        except (FileExistsError, OSError) as err:
-            logging.warning("reader error %r", err)
-        # test writer reader
-        logging.info("TESTING WRITER READER")
-        version = 4
-        filename = PathProvider().getFileName(
-            depId, contentType, milestone, partNumber, contentFormat, version
-        )
-        filepath = os.path.join(folder, filename)
-        try:
-            async with lock(filepath, "w", timeout=60):
-                testVal = 1
-                try:
-                    async with lock(filepath, "r", timeout=5):
-                        testVal = 0
-                except (FileExistsError, OSError) as err:
-                    logging.warning("writer error %r", err)
-                self.assertFalse(testVal == 0, "lock error - test val = 0")
-                self.assertTrue(
-                    testVal == 1, "lock error - test val %d" % testVal
-                )
-        except (FileExistsError, OSError) as err:
-            logging.warning("error %r", err)
+async def testLock(self, lock):
+    testVal = -1
+    repositoryType = "unit-test"
+    depId = "D_000"
+    contentType = "model"
+    milestone = "upload"
+    partNumber = 1
+    contentFormat = "pdbx"
+    version = 1
+    folder = PathProvider().getDirPath(repositoryType, depId)
+    self.assertTrue(folder is not None, "error - could not create folder")
+    logging.info("folder %s", folder)
+    filename = PathProvider().getFileName(
+        depId, contentType, milestone, partNumber, contentFormat, version
+    )
+    self.assertTrue(filename is not None, "error - could not create file name")
+    logging.info("file name %s", filename)
+    filepath = os.path.join(folder, filename)
+    logging.info("file path %s", filepath)
+    # test two writers
+    logging.info("TESTING TWO WRITERS")
+    try:
+        async with lock(filepath, "w", timeout=60):
+            testVal = 1
+            try:
+                async with lock(filepath, "w", timeout=5):
+                    testVal = 0
+            except (FileExistsError, OSError) as err:
+                logging.warning("writer error %r", err)
+            self.assertFalse(testVal == 0, "write lock error - test val = 0")
+            self.assertTrue(
+                testVal == 1, "write lock error - test val %d" % testVal
+            )
+    except (FileExistsError, OSError) as err:
+        logging.warning("writer error %r", err)
+    # test two readers
+    logging.info("TESTING TWO READERS")
+    version = 2
+    filename = PathProvider().getFileName(
+        depId, contentType, milestone, partNumber, contentFormat, version
+    )
+    filepath = os.path.join(folder, filename)
+    try:
+        # second traversal required to detect simultaneity otherwise ternary lock has infinite loop
+        async with lock(filepath, "r", timeout=60, second_traversal=True):
+            testVal = 1
+            try:
+                async with lock(filepath, "r", timeout=5, second_traversal=True):
+                    testVal = 0
+            except (FileExistsError, OSError) as err:
+                logging.warning("reader error %r", err)
+            self.assertFalse(testVal == 1, "read lock error - test val = 1")
+            self.assertTrue(
+                testVal == 0, "read lock error - non-zero test val %d" % testVal
+            )
+    except (FileExistsError, OSError) as err:
+        logging.warning("reader error %r", err)
+    # test reader writer
+    logging.info("TESTING READER WRITER")
+    version = 3
+    filename = PathProvider().getFileName(
+        depId, contentType, milestone, partNumber, contentFormat, version
+    )
+    filepath = os.path.join(folder, filename)
+    try:
+        # second traversal required to detect simultaneity otherwise ternary lock has infinite loop
+        async with lock(filepath, "r", timeout=60, second_traversal=True):
+            testVal = 1
+            try:
+                async with lock(filepath, "w", timeout=5, second_traversal=True):
+                    testVal = 0
+            except (FileExistsError, OSError) as err:
+                logging.warning("reader error %r", err)
+            self.assertFalse(testVal == 0, "read lock error - test val = 0")
+            self.assertTrue(
+                testVal == 1, "read lock error - test val %d" % testVal
+            )
+    except (FileExistsError, OSError) as err:
+        logging.warning("reader error %r", err)
+    # test writer reader
+    logging.info("TESTING WRITER READER")
+    version = 4
+    filename = PathProvider().getFileName(
+        depId, contentType, milestone, partNumber, contentFormat, version
+    )
+    filepath = os.path.join(folder, filename)
+    try:
+        async with lock(filepath, "w", timeout=60):
+            testVal = 1
+            try:
+                async with lock(filepath, "r", timeout=5):
+                    testVal = 0
+            except (FileExistsError, OSError) as err:
+                logging.warning("writer error %r", err)
+            self.assertFalse(testVal == 0, "lock error - test val = 0")
+            self.assertTrue(
+                testVal == 1, "lock error - test val %d" % testVal
+            )
+    except (FileExistsError, OSError) as err:
+        logging.warning("error %r", err)
 
 def tests():
     suite = unittest.TestSuite()
